b6292ced3ada2fdb1bf2af6dc23833ff6807377f
Devron Tombacco <tombacco.devron@gmail.com>
2026-02-23 18:17:47 +0000

    feat: add endpoints: /tickets (Get + POST)

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Welcome to the Ticketing System API'
 
+@app.route('/tickets', methods=['GET'])
+def returnAllTickets():
+    return 'Here are all tickets'
+
+@app.route('/tickets', methods=['POST'])
+def createTicket():
+    return 'Ticket created successfully'
+
 if __name__ == '__main__':
     app.run(debug=True)
